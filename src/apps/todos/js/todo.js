d31d550e5db317be23009d08094a6b36efee3ad3
Zach Lamb <zala3440@colorado.edu>
2016-01-18 17:44:03 -0700

    finished todos.js

--- src/apps/todos/js/todo.js
+++ src/apps/todos/js/todo.js
@@ -1,66 +1,17 @@
-$(document).ready(function() { 
-  var fire = new Firebase('https://ucdd2bookindividual.firebaseio.com')
+var filter = "all"
+var tasks = []
 
-  showPage("aboutme");
-  fire.child('resume/info').once('value', function(snapshot){
+$(document).ready(function() { 
+  $("#paralax").paralax()
+  var fire = new Firebase('https://ucdd2bookindividual.firebaseio.com')
+  fire.child('resume/info').on('value', function(snapshot){
     var info = snapshot.val();
+    tasks = info.tasks;
+    loadData(tasks,filter);
     console.log('Name: ' + info.name);
     $('#name').text(info.name);                      
   })
 });
-
-function showSuccess() {
-   $('#successAlert').show();
-   $('#warningAlert').hide();
-   $('#failureAlert').hide();
-}
-function showWarning() {
-   $('#warningAlert').show();
-   $('#successAlert').hide();
-   $('#failureAlert').hide();
-}
-function showError() {
-   $('#failureAlert').show();
-   $('#successAlert').hide();
-   $('#warningAlert').hide();
-}
-
-function showPage(file) {
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
-        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-            document.getElementById("presentation_content").innerHTML = xmlhttp.responseText;
-            
-            /*if (file == "low")
-            {
-              loadLow();
-              //LoadData
-            }
-            else if( file =='medium')
-            {
-              loadHigh();
-            }
-            else if( file =='high'){
-              loadExperienceData();
-            }
-            else if( file == ''){
-              loadSkillData();
-            }
-            else if( file == 'contact'){
-              loadContactData();
-            }
-            else{
-              loadAll();
-            }*/
-        
-        }
-        else{
-            document.getElementById("presentation_content").innerHTML = "<p>There was an error loading the page</p>"
-        }
-    }   
-    xmlhttp.open("GET", "./pages/"+file+".html", true);
-    xmlhttp.send();
-}
 
 function loadData(tasks,filter) 
   {
