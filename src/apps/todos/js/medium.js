36d0c94519b3c26180a455e425be4c101585750d
unknown <matt.bubernak@gmail.com>
2016-01-18 17:14:45 -0700

    added the filter data thing

---
+++ src/apps/todos/js/medium.js
@@ -0,0 +1,17 @@
+function loadData(tasks,filter) 
+	{
+		tasks.forEach(function(task){
+							if (task.priority == filter || task.priority == "All")
+							{
+			                    $('#tasks').append(
+			                        '<div class="col s12 m6">'+
+			                        '<div class="card blue-grey darken-1">'
+			                        +'<div class="card-content black-text">'+
+			                        '<span class="card-title collection-item '+task.priority + '">'+ task.title + '</span>'+'<p>Deadline: '+task.deadline+'        Priority: '+ task.priority +'  Type: '+ task.type +'</p>' + '</div>'
+			                        +'<div class="card-action">'+'<a href="#">Complete</a>\'</div>'+
+			                        
+			                        '</div>'+
+			                        '</div>')
+			                }
+			            })
+	}
