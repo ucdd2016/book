a3aa8bdbec33292c7478ffa765a0cc648af86420
CallanFisher <cafi6580@colorado.edu>
2016-01-20 16:57:25 -0700

    large icon

--- src/apps/todos/js/todo.js
+++ src/apps/todos/js/todo.js
@@ -45,7 +45,7 @@ function loadData(tasks,filter)
                               '<div class="col s12 m6">'+
                               '<div class="card blue-grey darken-1">'
                               +'<div class="card-content black-text">'+
-                              '<i class="meduim mdi-action-done-all"></i>'+
+                              '<i class="large mdi-action-done-all"></i>'+
                               '<span class="card-title collection-item '+task.priority + '">'+ task.title + '</span>'+'<p>Deadline: '+task.deadline+'        Priority: '+ task.priority +'  Type: '+ task.type +'</p>' + '</div>'
                               +'<div class="card-action">'+'<a href="#">Complete</a>\'</div>'+
                               
