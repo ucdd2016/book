c58a3de165631e94674e49480f1387e4c801a2ed
Nicolas Broeking <nbroeking@me.com>
2016-01-26 21:15:51 -0700

    Finished up garages_map

--- src/apps/parking/js/main.js
+++ src/apps/parking/js/main.js
@@ -10,20 +10,13 @@ $(document).ready(function() {
     showPage('garages');
   })
   $("#Map").click(function(){
-    showPage('garages_map');
-  })
+    showPage('garages_map', function() {
+      initMap();
+    });
+  });
+  
 });
 
-function showPage(file) {
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
-        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-            document.getElementById("presentation_content").innerHTML = xmlhttp.responseText;
-        }
-        else{
-            document.getElementById("presentation_content").innerHTML = "<p>There was an error loading the page</p>"
-        }
-    }   
-    xmlhttp.open("GET", "./pages/"+file+".html", true);
-    xmlhttp.send();
+function showPage(file, completedFunc) {
+  $( "#presentation_content" ).load( "./pages/" + file+".html", completedFunc);
 }
