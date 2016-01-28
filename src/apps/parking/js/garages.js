2b31da24e67484879669bf02f69e1c29a2804c7a
Frederik Lohner <freddylohner@gmail.com>
2016-01-28 16:14:22 -0700

    Minor changes, now displays information correctly for times

--- src/apps/parking/js/garages.js
+++ src/apps/parking/js/garages.js
@@ -1,9 +1,9 @@
 function initList(){
-  
+
   $('.collapsible').collapsible({
       accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
     });
-  
+
   // create a firebase reference to the root
   var ref = new Firebase('https://publicdata-parking.firebaseio.com');
 
@@ -12,7 +12,7 @@ function initList(){
   // read data from the location san_francisco/garages, only once
   ref.child('san_francisco/garages').once('value', function(snapshot){
     data = snapshot.val()
-    console.log('data is loaded', data)
+    // console.log('data is loaded', data)
 
     // filter the data
     var garages = _.filter(data, function(d){
@@ -20,14 +20,14 @@ function initList(){
       return _.has(d, 'open_spaces')
     })
 
-    console.log('garages is processed', garages);
-    
+    // console.log('garages is processed', garages);
+
     displayGarages(garages)
   })
 }
 function displayGarages(garages){
   $('#garages').empty();
-  
+
   // lodash _.forEach https://lodash.com/docs#forEach
   _.forEach(garages, function(val, key){
     $('#garages').append(generateListItem(val))
@@ -35,25 +35,25 @@ function displayGarages(garages){
 }
 
 function generateListItem(val){
-  
+
   var listItem = '<li>'
   listItem += '<div class="collapsible-header"><i class="material-icons">place</i>'
-  
+
   //Add header Here
   listItem += val['friendlyName'];
-  
+
   listItem += '</div>'
-  
+
   listItem += createSubListItem("Open Spaces: " + val['open_spaces'])
-  
+
   listItem += createSubListItem("Total Spaces: " + val['total_spaces'])
-   
+
   listItem += createSubListItem("Percentage Open: " + Math.floor(100*val['open_spaces'] / val['total_spaces']) + "%")
-    
+
   var hours = val['hours']
-  
+
   listItem += getHours(hours)
-  
+
   listItem += getCosts(val['rates'])
   return listItem
 }
@@ -67,23 +67,23 @@ function createSubListItem(str){
 
 function getHours(hours){
   var listItem = "";
-  
+
   var time = ((hours) ? hours['time'] : false)
-  
+
   if( hours.constructor == Array){
       listItem += '<div class="collapsible-body"><p>Hours of Operation: '
-      
+
       hours.forEach(function(entry){
         listItem += '<p>'+entry['BEG'] + " to " + entry['END']+ " " + entry['FROM'] ;
-        
+
         if( entry['TO']){
           listItem += " thru " + entry['TO']
         }
-        
+
         listItem += "</p>"
       })
-      
-      
+
+
       listItem += '</div>';
   }
   else{
@@ -101,25 +101,28 @@ function createSubListItem(str){
 
 function getCosts(costs){
   var listItem = "";
-  
-  console.log("costs: ", costs)
+
+  // console.log("costs: ", costs)
   //var time = ((hours) ? costs['time'] : false)
-  
-  if( costs == Array){
-      listItem += '<div class="collapsible-body"><p>Pricing: '
-      
+  console.log(costs);
+  if( costs.constructor == Array){
+      listItem += '<div class="collapsible-body"><p>Costs: '
+
       costs.forEach(function(entry){
-        listItem += '<p>'+costs['BEG'] + ((entry['END'] != null)? " to " + entry['END']: "")+ " is $" + entry['RATE'] + " " + entry['RQ'];
-        
-      
-        listItem += "</p>"
+        if (entry['BEG'] !== undefined) {
+          listItem += '<p>'+entry['BEG'] + " to " + entry['END'] + " Cost: $" + entry['RATE'] + " per " + entry['RQ'];
+          listItem += "</p>"
+        }
       })
-      
-      
+
+
       listItem += '</div>';
-  }
-  else{
-    listItem += createSubListItem("Pricing: " + costs['BEG'] + " " + costs['FROM'] + " is $" + costs['RATE'] + " " + costs['RQ'] )
+  } else{
+    if (costs['BEG'] === undefined){
+    return listItem
+    }
+
+    listItem += createSubListItem("Costs: " + costs['BEG'] + " " + costs['FROM'])
   }
   return listItem
 }
