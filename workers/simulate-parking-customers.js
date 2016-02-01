b76887248b72509c2062dea05f0f0653aa2090c6
Frederik Lohner <freddylohner@gmail.com>
2016-02-01 16:25:35 -0700

    Push, delete and clear now work

--- workers/simulate-parking-customers.js
+++ workers/simulate-parking-customers.js
@@ -4,57 +4,59 @@ var Firebase = require('firebase');
 
 // San Francisco
 var city_location = {
-  lat: 37.78,
-  lon: -122.41
+    lat: 37.78,
+    lon: -122.41
 }
 
 var radius = 0.03
 
 // simualate a random person entering, staying for a duration, and leaving
-function simulate(){
+function simulate() {
 
-  // generate a random person with a random name,
-  // random location, and random duration
-  var name = random_name()
-  var duration = 1 + 5 * Math.random()
-  var lat = city_location.lat + radius * (Math.random() - 0.5) * 2
-  var lon = city_location.lon + radius * (Math.random() - 0.5) * 2
-  var person = {
-    name: name,
-    duration: duration,
-    lat: lat,
-    lon: lon
-  }
+    // generate a random person with a random name,
+    // random location, and random duration
+    var name = random_name()
+    var duration = 1 + 5 * Math.random()
+    var lat = city_location.lat + radius * (Math.random() - 0.5) * 2
+    var lon = city_location.lon + radius * (Math.random() - 0.5) * 2
+    var person = {
+        name: name,
+        duration: duration,
+        lat: lat,
+        lon: lon
+    }
 
-  // simulate this person entering
-  enter(person)
+    // simulate this person entering
+    enter(person)
 
-  // simulate this person leaving after 'duration' seconds
-  setTimeout(function(){
-    leave(person)
-  }, duration * 1000)
+    // simulate this person leaving after 'duration' seconds
+    setTimeout(function() {
+        leave(person)
+    }, duration * 1000)
 
 }
 
-function enter(person){
-  console.log('enter', person)
-  // TODO: put this person in the Firebase
-  // var ref = new Firebase('your-firebase-url')
-  // ...
+function enter(person) {
+    console.log('enter', person)
+    var ref = new Firebase('https://ucdd2teamtwo.firebaseio.com');
+    var usersRef = ref.child("customers");
+    usersRef.child(person.name).set({
+        duration: person.duration,
+        lat: person.lat,
+        lon: person.lon
+    });
 }
 
-function leave(person){
-  console.log('leave', person)
-  // TODO: remove this person from the Firebase
-  // var ref = new Firebase('your-firebase-url')
-  // ...
+function leave(person) {
+    console.log('leave', person)
+    var ref = new Firebase('https://ucdd2teamtwo.firebaseio.com/customers/' + person.name);
+    ref.remove();
 }
 
 
-function clear(){
-  // TODO: remove all people from the Firebase
-  // var ref = new Firebase('your-firebase-url')
-  // ...
+function clear() {
+   var ref = new Firebase('https://ucdd2teamtwo.firebaseio.com/customers/');
+    ref.remove();
 }
 
 
