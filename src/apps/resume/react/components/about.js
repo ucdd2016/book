370306b992c6c45533c73d6cd6970ef8a3ab3144
Frederik Lohner <freddylohner@gmail.com>
2016-02-08 17:17:50 -0700

    Pushing week 5

--- src/apps/resume/react/components/about.js
+++ src/apps/resume/react/components/about.js
@@ -1,15 +1,31 @@
 MyComponents.About = React.createClass({
-
-  render: function() {
-    return (
-      <div className="card">
-        <div className="card-content">
-        TODO: This is a component to display the basic
-        information about myself such as name, profile photo,
-        major, birth place, ... etc.
+render: function() {
+return (
+<div className="card grey lighten-1">
+      <div className="row">
+        <div className="col s4">
+          <div className="card grey darken-1">
+            <div className="card-image">
+              <img src="https://avatars0.githubusercontent.com/u/3654470?v=3&s=460"/>
+            </div>
+          </div>
         </div>
-      </div>
-    );
-  }
 
+        <div className="col s8">
+          <div className="card grey lighten-1">
+            <div className="card-content black-text">
+              <span className="card-title">About Me</span>
+              <p>Frederik Lohner</p>
+              <p>Major: Computer Science</p>
+              <p>Minor: Economics</p>
+              <p><a href="https://github.com/FredLoh">Github</a></p>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+      </div>
+);
+}
 });
