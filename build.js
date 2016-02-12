2df735c341cd7d557e7f5f1e1261274b656547be
Rishab Kanwal <rishabkanwal@gmail.com>
2016-02-12 14:25:07 -0700

    Workers now work

--- build.js
+++ build.js
@@ -7,6 +7,7 @@ var branch = require('metalsmith-branch')
 var rename = require('metalsmith-rename')
 var inplace = require('metalsmith-in-place')
 var _ = require('lodash')
+var _ = require('chance')
 
 metalsmith(__dirname)
     .use(branch('**/*.md')
