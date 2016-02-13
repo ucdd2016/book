cbc460251217d3ee09a79e1efc4efe039893727a
Rishab Kanwal <rishabkanwal@gmail.com>
2016-02-12 21:44:47 -0700

    Cleaned up admin and index for Uber

--- src/apps/uber/admin/components/app.jsx
+++ src/apps/uber/admin/components/app.jsx
@@ -3,24 +3,21 @@ class App extends React.Component {
     console.log(this.props.data)
     return(
     <div className="section">
-    <nav>
-       <div className="nav-wrapper deep-orange lighten-1">
-       <a href="#" className="brand-logo">Admin</a>
-       <ul id="nav-mobile" className="right hide-on-med-and-down">
-          <li><a href="#restaurant">Restaurant</a></li>
-          <li><a href="#map">Map</a></li>          
-        </ul>
-       </div>
-    </nav>
+     <nav className="nav-wrapper">
+        <div className="nav-wrapper deep-orange lighten-1 navbar navbar-inverse navbar-fixed-top">
+            <ul id="nav-mobile navbar-inner">
+              <li className="left brand-logo" id="nav-title">fooBer Admin</li>      
+              <li className="right"><a href="#restaurant">Restaurant</a></li>
+              <li className="right"><a href="#map">Map</a></li>                    
+            </ul>
+        </div>
+      </nav>
       <div className="row">
       	<MyComponents.UserList users={this.props.data.users}/>
       	<MyComponents.ProviderList providers={this.props.data.drivers}/>
       	<MyComponents.UserMap users={this.props.data.users} providers={this.props.data.drivers} center={this.props.data.center}/>
       </div>   	
     </div>
-   
-
-   
     );
   }
 }
--- src/apps/uber/admin/components/user-list.jsx
+++ src/apps/uber/admin/components/user-list.jsx
@@ -3,12 +3,12 @@ MyComponents.user = React.createClass({
 		return( 
 		<div className="card-panel deep-orange lighten-1">
 			<div className="card-content">
-      			<span className="card-title text-darken-4"><h4>{this.props.user.restaurant_name}</h4></span>
-    			 <h7 className="blue-grey darken-4 white-text">Delivery location: {this.props.user.delivery_location}</h7>
+      			<span className="card-title white-text"><h4>{this.props.user.restaurant_name}</h4></span>
+    			 <h7>Delivery location: {this.props.user.delivery_location}</h7>
     	</div>
 			<div clasName="card-action">
-          <p>Order amount: {this.props.user.order_amount}</p>
-          <p>Time: {this.props.user.delivery_time}</p>
+          <p>Order amount: ${this.props.user.order_amount}</p>
+          <p>Time: {this.props.user.delivery_time} minutes</p>
       </div>
 		</div>
 		);
@@ -23,8 +23,7 @@ class UserList extends React.Component {
     return( 
     <div className="col s10" id="restaurant">
      <div className="icon-block">
-        <h2 className="center"><i className="material-icons">receipt</i></h2>
-        <h3 className="center">Orders</h3>
+        <h3 className="center white-text">Orders <i className="material-icons">receipt</i></h3>
  		{user}
      </div>      
     </div>
