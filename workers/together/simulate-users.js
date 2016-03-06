b673aa5a063a03ab8ecc05b2be5152c36033e79a
SerendpityZOEY <zoey.zhang513@hotmail.com>
2016-03-06 14:44:30 -0700

    canvas

--- workers/together/simulate-users.js
+++ workers/together/simulate-users.js
@@ -2,9 +2,9 @@ var _ = require('lodash')
 var Chance = require('chance')
 var random_name = require('node-random-name');
 var Firebase = require('firebase');
-//var ref_Group = new Firebase("https://wetravel.firebaseio.com/Groups")
+var ref_Group = new Firebase("https://wetravel.firebaseio.com/Groups")
 var ref_User = new Firebase("https://wetravel.firebaseio.com/Users")
-//get the current time
+//get the current time for message useage
 var d = new Date()
 var hour = d.getHours()
 var minute = d.getMinutes()
@@ -12,7 +12,7 @@ var time = hour+':'+minute
 
 // simualate a random person entering, staying for a duration, and leaving
 function simulate(){
-  // pick a random in firebase, and duratio
+  // random user name
   var chance = new Chance()
   var name = random_name()
   var duration = 1 + 5 * Math.random()
@@ -20,27 +20,47 @@ function simulate(){
     name: name,
     status: 'online'
   }
-   login(user)
-  //var groupName = chance.word() + " " + chance.word()
-  //duration = 1 + 5 * Math.random()
-
+  login(user)
+  joinGroup(user, 'CS_Grad_Trip')
+  // random message
+  var message = chance.word() + " " + chance.word() + " " +chance.word()
+  // random schedule
+  var Day = "Day" + duration.toString()
+  var budget = Math.random()*50+1
+  var place = chance.word() + " " + chance.word()
+  var startTime  = Math.floor(Math.random()*22)+1
+  var endTime  = startTime+2
+  var time = startTime.toString() + ':' + endTime.toString()
+  var traffic = ['Rent car', 'Taxi', 'Bus', 'Metro', 'Bike', 'Walk']
+  var transportation = traffic[Math.floor(Math.random()*traffic.length)]
+  var schedule = {
+    Day: Day,
+    budget: budget,
+    place: place,
+    time: time,
+    transportation: transportation
+  }
+  setTimeout(function(){
+    chat('CS_Grad_Trip',user, message, time)
+    addSchedule('CS_Grad_Trip', schedule)
+    canvas('CS_Grad_Trip')
+    //clickonMap()
+  }, duration * 1000)
 
   // simulate this person login
   // simulate this person logout after 'duration' seconds
-   setTimeout(function(){
-    user.status='offline'
+  setTimeout(function(){
     logout(user)
-   }, duration * 1000)
+  }, duration * 1000)
 
 }
-
 function login(user){
   console.log('login', user)
   ref_User.once('value', function(snapshot){
     var users = snapshot.val()
     users = Object.keys(users)
     if (user in users){
-      ref_User.child(user.name).update({status:'online'})
+      ref_User.child(user.name).update({status:online})
     }
     else{
       ref_User.child(user.name).set({
@@ -49,7 +69,7 @@ function login(user){
       })
     }
   })
-  
+
 }
 
 function logout(user){
@@ -57,6 +77,73 @@ function logout(user){
   ref_User.child(user.name).update({status: 'offline'});
 }
 
+function joinGroup(user, group){
+  ref_User.child(group).child('member').child(user.name).set({
+    name: user.name,
+    status: user.status
+  })
+}
+
+function chat(group,user, message, time){
+  console.log('chat', group,user,message,time)
+  var chance = new Chance()
+  ref_Group.child(group).child('Message').child(chance.word()).set({
+    message: message,
+    time: time,
+    username:user
+  })
+}
+
+function canvas(group){
+  var x = Math.floor(Math.random() * (120 + 1));
+  var y = Math.floor(Math.random() * (105 + 1));
+  var curColor = randColor();
+  var curSize = randSize();
+  var curTool = randTool();
+  if(curTool == 'eraser'){
+    curColor = 'fff';
+  }
+  ref_Group.child(group).child('drawing').child(x+':'+y).set({
+    curColor: curColor,
+    curSize : curSize,
+    curTool : curTool
+  })
+  console.log('canvas', x, ':', y, 'color:', curColor, 'size:', curSize, 'tool:', curTool)
+}
+
+
+function randSize(){
+  var size = [
+    'small',
+    'medium',
+    'large'
+  ];
+  var rand = Math.floor(Math.random()*size.length);
+  return size[rand]
+}
+
+function randTool(){
+  var tool = [
+    'eraser',
+    'marker'
+  ];
+  var rand = Math.floor(Math.random()*tool.length);
+  return tool[rand]
+}
+
+function randColor(){
+  var colors = ["fff","000","f00","0f0","00f","88f","f8d","f88","f05","f80","0f8","cf0","08f","408","ff8","8ff", "aed081", "eee"];
+
+  var rand = Math.floor(Math.random()*colors.length);
+  return colors[rand]
+}
+
+
+function addSchedule(group, schedule){
+
+}
+// function clickonMap()
+///-----------------------------------------------------------------------
 // function Make_Group(groupName, key, userName){
 
 //   ref_Group.child(groupName).set({
@@ -93,8 +180,10 @@ function logout(user){
 //     else{
 //       ref_User.child(userName).child('groups').set([groupName])
 //     }
-    
+
 // }
+
+
 
 // run each second
 setInterval(simulate, 2000)
