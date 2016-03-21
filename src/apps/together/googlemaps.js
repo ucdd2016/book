e031d21fe78f2d33ed74821b1bce8eeb724f8d59
SerendpityZOEY <zoey.zhang513@hotmail.com>
2016-03-20 18:33:14 -0600

    clean the code

--- src/apps/together/googlemaps.js
+++ src/apps/together/googlemaps.js
@@ -5,44 +5,25 @@ var gmap = {
     mapsensor:  "false",
     mapsize:    "480x420",
     urlpostfix: "",
-    maptype:    "map", // default to standard map
-    elem_map:   "",
 
     // Initialize the object
-    init: function(address,targetmap){
+    init: function(address){
 
-        if(! (address && targetmap)) {
+        if(! (address)) {
             console.log("init: all 3 parameters should be set");
             return;
         }
         // Set variables
         this.address = encodeURIComponent(address);
         this.urlpostfix = "&size="+this.mapsize+"&sensor="+this.mapsensor;
-        this.elem_map = $("#"+targetmap) || {};
-        // Render the elements
-        var res = this.render();
-        return res;
+
+        var inputurl = this.generateURL();
+        return inputurl;
     },
 
     // Generate the url used for the different map types
-    generateURL: function(t){
-        if(!t) return;
-        this.maptype = t;
-        switch (this.maptype){
-            case 'map':
-                var res = this.mapurl+this.address+this.urlpostfix;
-                return res;
-                break;
-        }
-    },
-
-    // Render image elements in targets
-    render: function(){
-        $elem_map = $(this.elem_map);
-        $elem_map.html(
-            $(document.createElement("img"))
-                .attr("src",this.generateURL("map"))
-        );
-        return this.generateURL("map");
+    generateURL: function(){
+        var res = this.mapurl+this.address+this.urlpostfix;
+        return res;
     }
 };
